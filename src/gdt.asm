a5de36066e10a6e503a951c6599e46ec00180537
Kayra <kayracizmeci@gmail.com>
2026-05-01 20:37:12 +0300

    feat: improve gdt correctness and addressing

--- src/gdt.asm
+++ src/gdt.asm
@@ -4,12 +4,11 @@ global gdt_load
 global stack_top
 
 section .bss
-align 16
 
 tss:
     resb 104
+    resb 8          ; explicit padding: 104+8=112 (7x16), stack_bottom stays 16-byte aligned
 
-align 16
 stack_bottom:
     resb 16384
 
@@ -21,8 +20,8 @@ align 16
 gdt:
     dq 0x0000000000000000
     dq 0x00AF9A000000FFFF
-    dq 0x00AF92000000FFFF
-    dq 0x00AFF2000000FFFF
+    dq 0x00CF92000000FFFF
+    dq 0x00CFF2000000FFFF
     dq 0x00AFFA000000FFFF
 
 align 16
@@ -32,37 +31,44 @@ tss_desc:
 
 gdt_end:
 
+[WARNING -reloc-abs-qword]
 gdtr:
     dw gdt_end - gdt - 1
     dq gdt
+[WARNING +reloc-abs-qword]
 
 section .text
+
+[WARNING -reloc-rel-dword]
 
 gdt_load:
     cld
 
+    lea rdi, [rel tss]
     xor rax, rax
-    lea rdi, [tss]
     mov rcx, 13
     rep stosq
 
-    mov rax, tss
-    mov rcx, rax
+    lea rdi, [rel tss]
+    lea rsi, [rel tss_desc]
+    mov rcx, rdi
 
-    mov word [tss_desc + 0], 103
-    mov word [tss_desc + 2], cx
+    mov word [rsi + 0], 103
+    mov word [rsi + 2], cx
 
     shr rcx, 16
-    mov byte [tss_desc + 4], cl
-    mov byte [tss_desc + 5], 0x89
-    mov byte [tss_desc + 6], 0x00
-    mov byte [tss_desc + 7], ch
+    mov byte [rsi + 4], cl
+    mov byte [rsi + 5], 0x89
+    mov byte [rsi + 6], 0x00
+    mov byte [rsi + 7], ch
 
+    mov rax, rdi
     shr rax, 32
-    mov dword [tss_desc + 8], eax
-    mov dword [tss_desc + 12], 0
+    mov dword [rsi + 8], eax
+    mov dword [rsi + 12], 0
 
-    lgdt [gdtr]
+    lea rax, [rel gdtr]
+    lgdt [rax]
 
     push 0x08
     lea rax, [rel .flush]
@@ -77,8 +83,9 @@ gdt_load:
     mov fs, ax
     mov gs, ax
 
-    mov rax, stack_top
-    mov [tss + 4], rax
+    lea rax, [rel stack_top]
+    lea rdi, [rel tss]
+    mov [rdi + 4], rax
 
     mov ax, 0x30
     ltr ax
